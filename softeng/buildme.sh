c8d004c517fcb2393f7ff4ad90bc597fc5ed611d
David García Garzón <voki@canvoki.net>
2019-12-06 04:42:20 +0100

    softeng: modern buildme

--- softeng/buildme.sh
+++ softeng/buildme.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-pandoc metadata.yaml *.md -t revealjs -o index.html --self-contained
+build() {
+	pandoc metadata.yaml *.md -f markdown+emoji -t revealjs -o index.html --self-contained
+}
+
+build
+firefox index.html &
+inotifywait -mr -e close_write  . --exclude pdf | while read path events file 
+do
+    (echo $file | grep md$ ) ||
+    (echo $file | grep png$ ) ||
+		continue
+    build
+done
+
+
